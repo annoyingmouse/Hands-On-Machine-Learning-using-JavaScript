686b982941091944c6d9d699bd8e3647347566c9
Dominic Myers <annoyingmouse@gmail.com>
2021-01-03 14:03:18 +0000

    Something or other

--- Setting Up Our Machine Learning Environment/index.js
+++ Setting Up Our Machine Learning Environment/index.js
@@ -2,6 +2,6 @@ const SLR = require('ml-regression').SLR
 const inputs = [80, 60, 10, 20, 30]
 const outputs = [20, 40, 30, 50, 60]
 
-let regression = new SLR(inputs, outputs)
+const regression = new SLR(inputs, outputs)
 
 console.log(regression.predict(80))
